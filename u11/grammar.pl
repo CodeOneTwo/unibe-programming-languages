91f76c4aa40de42ba1aaffe5cf9f0bdf98f9455b
Jan Dietrich <jan.dietrich@tecpose.com>
2021-05-27 22:54:12 +0200

    Add assignment 11

---
+++ u11/grammar.pl
@@ -0,0 +1,28 @@
+article --> [a].
+article --> [the].
+noun --> [girl].
+noun --> [boy].
+pronoun --> [that].
+pronoun --> [this].
+auxiliary --> [is].
+
+sentence --> subject_predicate.
+sentence --> subject_predicate_object.
+
+subject --> article, noun.
+subject --> pronoun.
+
+predicate --> auxiliary.
+predicate --> verb.
+
+object --> article, noun.
+
+subject_predicate --> subject, verb_sleep.
+verb_sleep --> [sleeps].
+
+subject_predicate_object --> pronoun, auxiliary, object.
+subject_predicate_object --> article, noun, verb_likes, object.
+verb_likes --> [likes].
+
+verb --> verb_likes.
+verb --> sleep.
